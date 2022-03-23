be714d7b66eb469dd79b0b4e7ec4ef2421d17a7f
pratik_tilekar <pratikajspire@gmail.com>
2022-03-23 18:13:01 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Netflix';
+  data=[]
+
+  constructor(http:HttpClient)
+  {
+    http.get('https://api.jikan.moe/v4/anime').subscribe((response:any)=> {
+      console.log(response)
+      this.data=response.data;
+    })
+  }
 }
